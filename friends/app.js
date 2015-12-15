971fd1d94138f5f1d6b726fb0476accbaae778c4
Phillip Puckett <phillippuckett88@gmail.com>
2015-12-14 17:07:27 -0700

    Friend Machine Project 5/6

--- friends/app.js
+++ friends/app.js
@@ -1,14 +1,1 @@
-var app = angular.module('facebookFriends', []) // Tells browser to create a new angular module called "facebookFriends".
-
-/*Angular API Reference docs.angularjs.org
-What was covered in lecture:
-ng-app
-ng-controller
-$scope
-ng-model
-{{ }}
-ng-repeat
-filter
-ng-show and ng-hide
-ng-if
-ng-click*/
+var faceBookFriendMachine = angular.module('facebookFriendMachine', [])
